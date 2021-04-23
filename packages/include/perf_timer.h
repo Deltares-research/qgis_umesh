d6db93a2c062edb384293141148895883110eb42
Jan Mooiman <jan.mooiman@deltares.nl>
2021-04-23 21:08:16 +0200

    Update perf_timer after review

--- packages/include/perf_timer.h
+++ packages/include/perf_timer.h
@@ -9,21 +9,24 @@
 #include <fstream>
 #include <string>
 #include <chrono>
+#include <iterator>
 
 #define DO_TIMING 0
 #if DO_TIMING == 1
 #   define TOSTRING(x) #x
-#   define START_TIMERN(x) TestTimer * tt = TestTimer::get_instance(); tt->start(TOSTRING(x), __FILE__, __FUNCTION__, __LINE__);
 #   define START_TIMER(x) tt = TestTimer::get_instance(); tt->start(TOSTRING(x), __FILE__, __FUNCTION__, __LINE__);
-#   define CLEAR_TIMER(x) if (tt->get_count() != 0){ tt->clear(); }
-#   define DELETE_TIMERN(x) TestTimer * tt = TestTimer::get_instance(); delete tt ;
+#   define START_TIMERN(x) TestTimer * tt = TestTimer::get_instance(); tt->start(TOSTRING(x), __FILE__, __FUNCTION__, __LINE__);
+#   define CLEAR_TIMER() if (tt->get_count() != 0){ tt->clear(); }
+#   define DELETE_TIMER() delete tt ;
+#   define DELETE_TIMERN() TestTimer * tt = TestTimer::get_instance(); delete tt ;
 #   define STOP_TIMER(x) tt->stop(TOSTRING(x));
-#   define PRINT_TIMERN(x) TestTimer * tt = TestTimer::get_instance(); tt->log(x);  // paramter x is a filename
 #   define PRINT_TIMER(x) tt->log(x);  // parameter x is a filename
+#   define PRINT_TIMERN(x) TestTimer * tt = TestTimer::get_instance(); tt->log(x);  // paramter x is a filename
 #else
 #   define START_TIMERN(x)
 #   define START_TIMER(x)
 #   define CLEAR_TIMER(x)
+#   define DELETE_TIMER(x)
 #   define DELETE_TIMERN(x)
 #   define STOP_TIMER(x)
 #   define PRINT_TIMERN(x)
@@ -34,25 +37,27 @@ using namespace std;
 
 class Timing {
 public:
-	bool running;
-	string name;
-	int level;
-	int line_number;
-	int nr_calls;
-	double elapse_time;
-	string filename;
-	string functionname;
-	std::chrono::high_resolution_clock::time_point start;
-	std::chrono::high_resolution_clock::time_point stop;
-	Timing::Timing(const std::string& name,
-		const std::string& fileName,
-		const std::string& functionName,
-		int lineNumber) :
-		level(level), name(name), filename(fileName), functionname(functionName), line_number(lineNumber)
-	{
-		start = std::chrono::high_resolution_clock::now();
-		nr_calls = 1;
-	}
+    Timing() = default;
+    Timing(std::string name,
+        std::string fileName,
+        std::string functionName,
+        int lineNumber) :
+        level(level), name(std::move(name)), filename(std::move(fileName)), functionname(std::move(functionName)), line_number(lineNumber)
+    {
+        start = std::chrono::high_resolution_clock::now();
+        nr_calls = 1;
+    }
+
+    bool running;
+    string name;
+    int level;
+    int line_number;
+    int nr_calls;
+    double elapse_time;
+    string filename;
+    string functionname;
+    std::chrono::high_resolution_clock::time_point start;
+    std::chrono::high_resolution_clock::time_point stop;
 };
 
 
@@ -60,42 +65,42 @@ public:
 class TestTimer
 {
 public:
-	static TestTimer* get_instance()
-	{
-		if (obj == nullptr)
-		{
-			obj = new TestTimer(string("root"),
-				string("---"),
-				string("---"),
-				0);
-			obj->m_level = 0;
-		}
-		object_count = 1;
-		return obj;
-	}
+    static TestTimer* get_instance()
+    {
+        if (obj == nullptr)
+        {
+            obj = new TestTimer(string("root"),
+                string("---"),
+                string("---"),
+                0);
+            obj->m_level = 0;
+        }
+        object_count = 1;
+        return obj;
+    }
 
-	int get_count();
-	void log(string);
-	vector<Timing*> timing;
+    int get_count();
+    void log(string);
+    vector<Timing> timing;
 
-	TestTimer(const std::string& name, 
-		const std::string& fileName,
-		const std::string& functionName,
-		int lineNumber);
+    TestTimer(const std::string& name, 
+        const std::string& fileName,
+        const std::string& functionName,
+        int lineNumber);
 
-	void clear();
-	void TestTimer::start(const std::string& name, const std::string& fileName, const std::string& functionName, const int lineNumber);
-	void stop(const std::string& name);
-	~TestTimer();
+    ~TestTimer() = default;
 
-	private:
-		static TestTimer * obj;
-		static int object_count;
+    void clear();
+    void start(const std::string& name, const std::string& fileName, const std::string& functionName, int lineNumber);
+    void stop(const std::string& name);
 
-		int m_level;
-		std::string m_name;
-		std::string m_filename;
-		std::string m_functionname;
-		int m_line_number;
-		std::chrono::high_resolution_clock::time_point sstart;
+    private:
+        static TestTimer * obj;
+        static int object_count;
+
+        int m_level;
+        std::string m_name;
+        std::string m_filename;
+        std::string m_functionname;
+        int m_line_number;
 };
--- packages/src/perf_timer.cpp
+++ packages/src/perf_timer.cpp
@@ -15,154 +15,144 @@
 using namespace std;
 
 int TestTimer::object_count = 0;
-TestTimer* TestTimer::obj;  // Initialize static member of class System
+TestTimer* TestTimer::obj = nullptr;  // Initialize static member of class System
 
 TestTimer::TestTimer(const std::string& name, const std::string& fileName, const std::string& functionName, const int lineNumber) :
-	m_name(name), m_filename(fileName), m_functionname(functionName), m_line_number(lineNumber)
+    m_name(name), m_filename(fileName), m_functionname(functionName), m_line_number(lineNumber)
 {
-	if (object_count == 0)
-	{
-		object_count = 1;
-		m_level = 0;
-		timing.push_back(new Timing(m_name, m_filename, m_functionname, m_line_number));
-		timing[0]->level = m_level;
-		timing[0]->running = false;
-		timing[0]->elapse_time = 0.0;
-	}
-}
-TestTimer::~TestTimer()
-{
-	if (object_count == 1)
-	{
-		for (int i = 0; i < timing.size() - 1; i++)
-		{
-			delete timing[i];
-		}
-	}
+    if (object_count == 0)
+    {
+        object_count = 1;
+        m_level = 0;
+        timing.emplace_back(m_name, m_filename, m_functionname, m_line_number);
+        timing.back().level = m_level;
+        timing.back().running = false;
+        timing.back().elapse_time = 0.0;
+    }
 }
 int TestTimer::get_count()
 {
-	return object_count;
+    return object_count;
 }
 void TestTimer::clear()
 {
-	if (object_count == 1)
-	{
-		object_count = 0;
-		m_level = 0;
-		for (int i = 1; i < timing.size() - 1; i++)  // skip the first one; it is the root and has level zero
-		{
-			delete timing[i];
-		}
-	}
-	timing.resize(1);
+    if (object_count == 1)
+    {
+        object_count = 0;
+        m_level = 0;
+    }
+    timing.resize(1);
 }
 void TestTimer::start(const std::string& name, const std::string& fileName, const std::string& functionName, const int lineNumber)
 {
-	bool found = false;
-	for (int j = 0; j < timing.size(); j++)
-	{
-		if (timing[j]->name == name)
-		{
-			timing[j]->start = std::chrono::high_resolution_clock::now();
-			timing[j]->running = true;
-			timing[j]->nr_calls += 1;
-			m_level += 1;
-			found = true;
-			break;
-		}
-	}
-	if (!found) 
-	{
-		timing.push_back(new Timing(name, fileName, functionName, lineNumber));
-		m_level += 1;
-		timing[timing.size() - 1]->level = m_level;
-		timing[timing.size() - 1]->running = true;
-		timing[timing.size() - 1]->elapse_time = 0.0;
-	}
+    bool found = false;
+    for (auto& t : timing)
+    {
+        if (t.name == name)
+        {
+            t.start = std::chrono::high_resolution_clock::now();
+            t.running = true;
+            t.nr_calls += 1;
+            m_level += 1;
+            found = true;
+            break;
+        }
+    }
+    if (!found)
+    {
+        timing.emplace_back(Timing(name, fileName, functionName, lineNumber));
+        m_level += 1;
+        timing.back().level = m_level;
+        timing.back().running = true;
+        timing.back().elapse_time = 0.0;
+    }
 }
 void TestTimer::stop(const std::string& name)
 {
-	for (int i = 0; i < timing.size(); i++)
-	{
-		if (timing[i]->name == name)
-		{
-			m_level -= 1;
-			timing[i]->running = false;
-			timing[i]->stop = std::chrono::high_resolution_clock::now();
-			std::chrono::high_resolution_clock::duration d = timing[i]->stop - timing[i]->start;
-			timing[i]->elapse_time += d.count() * (double)std::chrono::high_resolution_clock::duration::period::num / std::chrono::high_resolution_clock::duration::period::den;
-		}
-	}
+    for (auto& t : timing)
+    {
+        if (t.name == name)
+        {
+            m_level -= 1;
+            t.running = false;
+            t.stop = std::chrono::high_resolution_clock::now();
+            std::chrono::high_resolution_clock::duration d = t.stop - t.start;
+            t.elapse_time += d.count() * (double)std::chrono::high_resolution_clock::duration::period::num / std::chrono::high_resolution_clock::duration::period::den;
+        }
+    }
 }
 void TestTimer::log(string out_file)
 {
-	if (timing.size() > 1)
-	{
-		std::ofstream os;
-		os.open(out_file);
-		for (int i = 0; i < timing.size(); i++)
-		{
-			if (timing[i]->running)
-			{
-				timing[i]->running = false;
-				timing[i]->stop = std::chrono::high_resolution_clock::now();
-				std::chrono::high_resolution_clock::duration d = timing[i]->stop - timing[i]->start;
-				timing[i]->elapse_time += d.count() * (double)std::chrono::high_resolution_clock::duration::period::num / std::chrono::high_resolution_clock::duration::period::den;
-			}
-		}
+    if (timing.size() > 1)
+    {
+        std::ofstream os;
+        os.open(out_file);
+        for (auto& t : timing)
+        {
+            if (t.running)
+            {
+                t.running = false;
+                t.stop = std::chrono::high_resolution_clock::now();
+                std::chrono::high_resolution_clock::duration d = t.stop - t.start;
+                t.elapse_time += d.count() * (double)std::chrono::high_resolution_clock::duration::period::num / std::chrono::high_resolution_clock::duration::period::den;
+            }
+        }
 
-		// determine string length, needed for nice layout
-		size_t name_strlen = 4;
-		size_t file_strlen = 4;
-		size_t function_strlen = 8;
-		for (int i = 0; i < timing.size(); i++)
-		{
-			name_strlen = max(name_strlen, timing[i]->name.length());
-			std::string file_str = timing[i]->filename.substr(timing[i]->filename.find_last_of("/\\") + 1);
-			file_strlen = max(file_strlen, file_str.length());
-			function_strlen = max(function_strlen, timing[i]->functionname.length());
-		}
-		size_t total_strlen = 5 + 3 +  // level
-			5 + 3 +  // calls
-			10 + 3 +  // time[s]
-			name_strlen + 3 +  // name
-			file_strlen + 3 +  // file
-			function_strlen + 3 +  //function name
-			6;  // line number
+        // determine string length, needed for nice layout
+        size_t name_strlen = 4;
+        size_t file_strlen = 4;
+        size_t function_strlen = 8;
+        for (auto& t : timing)
+        {
+            name_strlen = max(name_strlen, t.name.length());
+            std::string file_str = t.filename.substr(t.filename.find_last_of("/\\") + 1);
+            file_strlen = max(file_strlen, file_str.length());
+            function_strlen = max(function_strlen, t.functionname.length());
+        }
+        size_t const total_strlen = 5 + 3 +  // level
+            5 + 3 +  // calls
+            10 + 3 +  // time[s]
+            name_strlen + 3 +  // name
+            file_strlen + 3 +  // file
+            function_strlen + 3 +  //function name
+            6;  // line number
 
-		os << std::string(total_strlen, '=') << std::endl;
-		os << "level" << "   " << "calls" << "   "
-			<< right << setw(10) << " time[s]"
-			<< "   "
-			<< left << std::setw(name_strlen) << "name"
-			<< "   "
-			<< left << std::setw(file_strlen) << "file"
-			<< "   "
-			<< left << std::setw(function_strlen) << "function"
-			<< "   "
-			<< right << std::setw(6) << "line"
-			<< std::endl;
-		os << std::string(total_strlen, '-') << std::endl;
-		for (int i = 1; i < timing.size(); i++)  // skip the first one; it is the root and has level zero
-		{
-			std::string file_str = timing[i]->filename.substr(timing[i]->filename.find_last_of("/\\") + 1);
-			os << std::setw(5) << timing[i]->level
-				<< "   "
-				<< std::setw(5) << timing[i]->nr_calls
-				<< "   "
-				<< std::setw(10) << fixed << setprecision(3) << timing[i]->elapse_time
-				<< "   "
-				<< left << std::setw(name_strlen) << timing[i]->name
-				<< "   "
-				<< left << std::setw(file_strlen) << file_str
-				<< "   "
-				<< left << std::setw(function_strlen) << timing[i]->functionname
-				<< "   "
-				<< right << std::setw(6) << timing[i]->line_number
-				<< std::endl;
-		}
-		os << std::string(total_strlen, '=') << std::endl;
-		os.close();
-	}
+        os << std::string(total_strlen, '=') << std::endl;
+        os << "level" << "   " << "calls" << "   "
+            << right << setw(10) << " time[s]"
+            << "   "
+            << left << std::setw(name_strlen) << "name"
+            << "   "
+            << left << std::setw(file_strlen) << "file"
+            << "   "
+            << left << std::setw(function_strlen) << "function"
+            << "   "
+            << right << std::setw(6) << "line"
+            << std::endl;
+        os << std::string(total_strlen, '-') << std::endl;
+        for (auto& t : timing)
+        {
+            if (t.name == "root") 
+            { 
+                continue;  // skip the first one
+            }
+            std::string file_str = t.filename.substr(t.filename.find_last_of("/\\") + 1);
+            os << std::setw(5) << t.level
+                << "   "
+                << std::setw(5) << t.nr_calls
+                << "   "
+                << std::setw(10) << fixed << setprecision(3) << t.elapse_time
+                << "   "
+                << left << std::setw(name_strlen) << t.name
+                << "   "
+                << left << std::setw(file_strlen) << file_str
+                << "   "
+                << left << std::setw(function_strlen) << t.functionname
+                << "   "
+                << right << std::setw(6) << t.line_number
+                << std::endl;
+        }
+        os << std::string(total_strlen, '=') << std::endl;
+        os.close();
+    }
 }
