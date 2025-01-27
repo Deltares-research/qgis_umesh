dba919fa6a97a351afbed9185149e9fde075f62c
Jan Mooiman <mooiman@users.noreply.github.com>
2025-01-27 19:59:47 +0100

    Update for QGIS 3.40.3

--- dispatch_debug_dll_to_qgis.bat
+++
@@ -1,2 +0,0 @@
-copy  .\bin\x64\Debug\qgis_umesh.dll c:\OSGeo4W64\apps\qgis\plugins\*.*
-copy  .\bin\x64\Debug\qgis_umesh.pdb c:\OSGeo4W64\apps\qgis\plugins\*.*
--- dispatch_release_dll_to_qgis.bat
+++ dispatch_release_dll_to_qgis.bat
@@ -1,13 +1,13 @@
 rem copy  .\bin\x64\Release\qgis_umesh.dll c:\OSGeo4W64\apps\qgis\plugins\*.*
-copy  .\bin\x64\Release\qgis_umesh.pdb c:\OSGeo4W\apps\qgis\plugins\*.*
+copy  .\bin\x64\Release\qgis_sumesh.pdb c:\OSGeo4W\apps\qgis\plugins\*.*
 
 copy .\doc\qgis_umesh_um.pdf            "c:\Program Files\Deltares\qgis_umesh\doc\qgis_umesh_um.pdf"
 copy .\doc\qgis_umesh_installation.pdf  "c:\Program Files\Deltares\qgis_umesh\doc\qgis_umesh_installation.pdf"
 copy .\icons\*.*                        "c:\Program Files\Deltares\qgis_umesh\icons\*.*"
 
 rem copy to Bulletin
-copy "c:\OSGeo4W\apps\qgis\plugins\qgis_umesh.dll"      n:\Deltabox\Bulletin\mooiman\programs\QGIS_3.28.03_qgis_umesh\*.*
-copy .\doc\qgis_umesh_um.pdf                            n:\Deltabox\Bulletin\mooiman\programs\QGIS_3.28.03_qgis_umesh\doc\*.*
-copy .\doc\qgis_umesh_installation.pdf                  n:\Deltabox\Bulletin\mooiman\programs\QGIS_3.28.03_qgis_umesh\doc\*.*
-copy .\icons\*.*                                        n:\Deltabox\Bulletin\mooiman\programs\QGIS_3.28.03_qgis_umesh\icons\*.*
+copy "c:\OSGeo4W\apps\qgis\plugins\qgis_sumesh.dll"      n:\Deltabox\Bulletin\mooiman\programs\QGIS_3.40.03_qgis_umesh\*.*
+copy .\doc\qgis_umesh_um.pdf                            n:\Deltabox\Bulletin\mooiman\programs\QGIS_3.40.03_qgis_umesh\doc\*.*
+copy .\doc\qgis_umesh_installation.pdf                  n:\Deltabox\Bulletin\mooiman\programs\QGIS_3.40.03_qgis_umesh\doc\*.*
+copy .\icons\*.*                                        n:\Deltabox\Bulletin\mooiman\programs\QGIS_3.40.03_qgis_umesh\icons\*.*
 rem pause
